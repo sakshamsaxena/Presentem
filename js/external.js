d122e0d0b40bed6cc372eee4f3563e40f6eaa7fe
sakshamsaxena <ontenth@live.com>
2015-06-24 00:03:54 +0530

    Demo is now customizable

--- js/external.js
+++ js/external.js
@@ -1,5 +1,18 @@
 $(window).load(function(){
+	var wid=500,ht=500;
+	$("button").click(function(){
+		if($("#width").val() && $("#height").val()) {
+		wid = $("#width").val();
+		ht = $("#height").val();
+		alert("You've set the frame dimensions. Now click on an image to launch gallery.");}
+		else {
+		alert("You haven't set the frame dimensions. Using default dimensions (500,500)")}
+		return wid,ht;
+	})
     $("div#outer_ring img").click(function() {
-        $("div#outer_ring img").presentem();
+        $("div#outer_ring img").presentem({fWidth:wid, fHeight:ht});
     });
+	
+	
 });
+
